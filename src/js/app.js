8b414fb70c2226fe7ceb55c9238792e216632b3d
ptrv-dev <petrov99119@gmail.com>
2022-08-19 16:37:11 +0300

    Section Travel is done.

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,25 @@ const burgerButton = document.querySelector(".header__burger");
 const nav = document.querySelector("nav.nav");
 
 burgerButton.addEventListener("click", () => {
-	document.body.classList.toggle("no-scroll")
-    nav.classList.toggle("nav_active");
-    burgerButton.classList.toggle("header__burger_active")
+	document.body.classList.toggle("no-scroll");
+	nav.classList.toggle("nav_active");
+	burgerButton.classList.toggle("header__burger_active");
 });
+
+// Interactive map adaptive
+const map = document.querySelector(".map-travel");
+
+function mapScale() {
+	if (window.innerWidth < 520) {
+		map.style.transform = `scale(${innerWidth / 520})`;
+	} else {
+		map.style.transform = "";
+	}
+}
+
+window.addEventListener("resize", () => {
+	mapScale();
+});
+window.addEventListener("load", () => {
+	mapScale();
+})
