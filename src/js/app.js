6e12fdd22dadfe145227b6afa8ff0e0351d3870c
ptrv-dev <petrov99119@gmail.com>
2022-08-24 13:11:48 +0300

    Added some Pop Ups + little fixes

--- src/js/app.js
+++ src/js/app.js
@@ -24,4 +24,50 @@ window.addEventListener("resize", () => {
 });
 window.addEventListener("load", () => {
 	mapScale();
-})
+});
+
+// Pop Up
+
+const popUp = document.querySelector(".pop-up");
+const popUpClose = document.querySelectorAll("button.pop-up__close");
+
+let currentPopUp = undefined;
+
+const popUpShow = (id) => {
+	popUp.classList.add("_active");
+	document.body.classList.add("no-scroll");
+	document.querySelector(`#${id}`).classList.add("_active");
+};
+
+const popUpHide = () => {
+	popUp.classList.remove("_active");
+	document.body.classList.remove("no-scroll");
+	document.querySelector(`#${currentPopUp}`).classList.remove("_active");
+	currentPopUp = undefined;
+};
+
+popUpClose.forEach((e) => {
+	e.addEventListener("click", () => {
+		popUpHide();
+	});
+});
+
+document.body.addEventListener("keydown", (e) => {
+	if (e.key ==="Escape"){
+		if (currentPopUp != undefined) {
+			popUpHide();
+		}
+	}
+});
+
+document
+	.querySelector("button.bottom-travel-video__button")
+	.addEventListener("click", () => {
+		popUpShow("video-pop-up");
+		currentPopUp = "video-pop-up";
+	});
+
+document.querySelector("button.header__user").addEventListener("click", ()=> {
+	popUpShow("login-pop-up");
+	currentPopUp = "login-pop-up";
+});
